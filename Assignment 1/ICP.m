5528917390e333bb63178e72c8a4b6262c0e380d
Michelle <appel.michelle@gmail.com>
2018-04-11 15:50:09 +0200

    ICP

--- Assignment 1/ICP.m
+++ Assignment 1/ICP.m
@@ -1,5 +1,6 @@
 function result = ICP(point_clouds_path)
 % Input: point_clouds path
+% Example: ICP('Data/data/0000000000_normal.pcd')
 
 % Path to the supplemental code
 addpath SupplementalCode/
@@ -9,7 +10,7 @@ data = readPcd(point_clouds_path);
 
 [ datapoints, dimension ] = size(data);
 
-R = eye(datapoints);
+R = eye(dimension);
 t = 0;
 
 result = 0;
