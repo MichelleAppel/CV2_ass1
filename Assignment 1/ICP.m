6c0688a797703dbb65c6fe87de176f03d087cec2
Michelle <appel.michelle@gmail.com>
2018-04-12 21:12:11 +0200

    comments

--- Assignment 1/ICP.m
+++ Assignment 1/ICP.m
@@ -1,30 +1,29 @@
 function [R, t] = ICP()
 % Input: point_clouds path
 % Example: ICP('Data/data/0000000000_normal.pcd')
-clear
-clc
-close ALL
+
 % Read the data
 A1 = load('Data/source.mat');
 A1 = A1.source.';
 A2 = load('Data/target.mat');
 A2 = A2.target.';
+
 % Visualize both datasets using 3d scatter plot
 figure, scatter3(A1(:, 1),A1(:, 2),A1(:, 3)), title('A1 START')
 figure, scatter3(A2(:, 1),A2(:, 2),A2(:, 3)), title('A2 START')
     
 % Init
-R = eye(size(A1, 2));
-t = 0;
+R = eye(size(A1, 2)); % Initial rotation    of 0
+t = 0;                % Initial translation of 0
 
 % Find the closest point in A2 for each point in A1
 [~, phi] = pdist2(A1, A2, 'euclidean', 'Smallest', 1);
 phi = phi.';
-prev_rms = 69.42;
+prev_rms = 69.42; % Intial RMS
 
 counter = 0;
-% while RMS changes, update R and t
-while RMS(A1, A2, phi) ~= prev_rms && counter < 50
+% while RMS hasn't converged, update R and t
+while RMS(A1, A2, phi) <= prev_rms
     counter = counter + 1;
     disp("Counter")
     disp(counter)
@@ -34,7 +33,7 @@ while RMS(A1, A2, phi) ~= prev_rms && counter < 50
     phi = phi.';
 
     % Compute the weighted centroids of both point sets:
-    w = ones(size(A1, 1), 1);
+    w = ones(size(A1, 1), 1); % Set all weights to 1
     p_ = w.' * A1 / sum(w);
     q_ = w.' * A2 / sum(w);
    
@@ -72,7 +71,7 @@ figure, scatter3(A2(:, 1),A2(:, 2),A2(:, 3)), title('A2 END')
 
 end
 
+% Root Mean Square
 function rms = RMS(A1, A2, phi)
     rms = sqrt(1/size(A1, 1) * sum(sqrt(sum(A1 - A2(phi)).^2)));
 end
-
